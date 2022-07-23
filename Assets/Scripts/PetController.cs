952b05d53288db3defe3f54b89c9496e0b92da41
June <cba0898@gmail.com>
2022-07-23 16:28:51 +0900

    Add Pet's Animation

--- Assets/Scripts/PetController.cs
+++ Assets/Scripts/PetController.cs
@@ -8,6 +8,27 @@ public class PetController : MonoBehaviour
 
     void Awake()
     {
+
     }
 
+    public void Happy()
+    {
+        petAnimator.SetTrigger("Happy");
+    }
+    public void Hungry()
+    {
+        petAnimator.SetTrigger("Hungry");
+    }
+    public void Sad()
+    {
+        petAnimator.SetTrigger("Sad");
+    }
+    public void Tired()
+    {
+        petAnimator.SetTrigger("Tired");
+    }
+    public void Eat()
+    {
+        petAnimator.SetTrigger("Eat");
+    }
 }
